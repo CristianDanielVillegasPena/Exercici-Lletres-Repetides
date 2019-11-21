54b2cff01b54d6041a7b0240282dfb0935d82ea8
crisvil <utucrisvil@gmail.com>
2019-11-21 12:10:10 +0100

    Fase1

---
+++ FASE1/src/TablaChar.java
@@ -0,0 +1,16 @@
+
+public class TablaChar {
+
+	public static void main(String[] args) {
+		
+		char [] nombre = {'C', 'R', 'I', 'S', 'T', 'I', 'A', 'N'};
+		
+		for (int i=0; i<8; i++);
+		System.out.println(nombre);
+//---------------------------------------------------------------------//	
+		for (int i=0; i<8; i++){
+			System.out.println(nombre[i]);
+		}
+	}
+
+}
